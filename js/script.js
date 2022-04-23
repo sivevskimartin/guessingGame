240020eaa702f344008f828322a9c8fbf42af04e
sivevskimartin <63673317+sivevskimartin@users.noreply.github.com>
2022-04-23 19:43:47 +0200

    newGame

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var randNum = Math.floor(Math.random()*100+1);
+let guesses = 9;
+console.log(randNum);
+console.log(guesses);
+document.getElementById("btn").onclick = function() {
+
+        document.getElementById("lab2").innerHTML = guesses;
+        let guess = document.getElementById("typeNumber").value;
+        if(guesses==1)
+        {
+            alert("ИЗГУБИВТЕ ПРОБАЈТЕ УШТЕ ЕДНАШ...РЕФРЕШНЕТЕ ЈА СТРАНАТА");
+        }
+        else if(guess==randNum)
+        {
+            alert("ЧЕСТИТКИ ПОГОДИВТЕ...ИГРАТА ЗАВРШИ");
+        }
+        else if(guess>randNum)
+        {
+            alert("ПРОБАЈ ПОМАЛ БРОЈ");
+            guesses-=1;
+        }
+        else if(guess<randNum)
+        {
+            alert("ПРОБАЈ ПОГОЛЕМ БРОЈ");
+            guesses-=1;
+        }
+       
+}

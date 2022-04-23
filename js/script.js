1a93405df8804f710a07298b46535806ba15d6a8
sivevskimartin <63673317+sivevskimartin@users.noreply.github.com>
2022-04-23 20:47:09 +0200

    refres page in js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,12 @@ document.getElementById("btn").onclick = function() {
         if(guesses==1)
         {
             alert("ИЗГУБИВТЕ ПРОБАЈТЕ УШТЕ ЕДНАШ...РЕФРЕШНЕТЕ ЈА СТРАНАТА");
+            
         }
         else if(guess==randNum)
         {
             alert("ЧЕСТИТКИ ПОГОДИВТЕ...ИГРАТА ЗАВРШИ");
+            location.reload();
         }
         else if(guess>randNum)
         {
